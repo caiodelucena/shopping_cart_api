13d460343f5419140f701d3da7f52126c9c941d2
Caio Lucena <caiodelucena6@gmail.com>
2025-08-05 22:59:29 -0300

    test: update CartsController route tests

--- spec/routing/carts_routing_spec.rb
+++ spec/routing/carts_routing_spec.rb
@@ -3,15 +3,19 @@ require "rails_helper"
 RSpec.describe CartsController, type: :routing do
   describe 'routes' do
     it 'routes to #show' do
-      expect(get: '/cart').to route_to('carts#show')
+      expect(get: '/cart').to route_to('carts#show', format: :json)
     end
 
     it 'routes to #create' do
-      pending "#TODO: Escreva um teste para validar a criação de um carrinho #{__FILE__}" 
+      expect(post: '/cart').to route_to('carts#create', format: :json)
     end
 
     it 'routes to #add_item via POST' do
-      expect(post: '/cart/add_item').to route_to('carts#add_item')
+      expect(post: '/cart/add_item').to route_to('carts#add_item', format: :json)
+    end
+
+    it 'routes to #remove_item via DELETE' do
+      expect(delete: '/cart/1').to route_to('carts#remove_item', product_id: '1', format: :json)
     end
   end
 end 

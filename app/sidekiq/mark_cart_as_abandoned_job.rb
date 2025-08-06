992379a93284547f5fab901a561cd78a37ae462c
Caio Lucena <caiodelucena6@gmail.com>
2025-08-05 22:55:31 -0300

    feat: implement job logic to mark and remove abandoned carts

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,32 @@
+require 'sidekiq-scheduler'
+
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  queue_as :default
+
+  def perform
+    mark_abandoned_carts
+    delete_abandoned_carts
+  end
+
+  def mark_abandoned_carts
+    carts = Cart.find_inactive_carts_for_abandonment
+
+    return if carts.empty?
+
+    carts.update_all(status: :abandoned)
+  end
+
+  def delete_abandoned_carts
+    carts = Cart.find_expired_abandoned_carts
+
+    return if carts.empty?
+
+    carts.find_each do |cart|
+      cart.remove_if_abandoned
+    rescue StandardError => e
+      Rails.logger.error "Failed to remove abandoned cart #{cart.id}: #{e.message}"
+    end
   end
 end

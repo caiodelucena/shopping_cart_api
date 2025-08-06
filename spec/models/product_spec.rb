d6a2e2635039c9e58aa521d142c65087c98a06fb
Caio Lucena <caiodelucena6@gmail.com>
2025-08-05 22:58:01 -0300

    test: add rspec to Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  describe '#associations' do
+    it { is_expected.to have_many(:cart_items).dependent(:destroy) }
+    it { is_expected.to have_many(:carts).through(:cart_items) }
+  end
+
   context 'when validating' do
     it 'validates presence of name' do
       product = described_class.new(price: 100)
-      expect(product.valid?).to be_falsey
+      expect(product).not_to be_valid
       expect(product.errors[:name]).to include("can't be blank")
     end
 
     it 'validates presence of price' do
       product = described_class.new(name: 'name')
-      expect(product.valid?).to be_falsey
+      expect(product).not_to be_valid
       expect(product.errors[:price]).to include("can't be blank")
     end
 
     it 'validates numericality of price' do
       product = described_class.new(price: -1)
-      expect(product.valid?).to be_falsey
+      expect(product).not_to be_valid
       expect(product.errors[:price]).to include("must be greater than or equal to 0")
     end
   end
